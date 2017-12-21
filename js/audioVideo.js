a98037944c56aa8dc4c655ec8ab9057da7651d96
David Siguenza Rodriguez <dsiguenzarod@gmail.com>
2017-12-21 17:51:51 +0100

    cambios

--- js/audioVideo.js
+++ js/audioVideo.js
@@ -1,30 +1,72 @@
 
 var vid = document.getElementById("vid1");
+var enableControls = document.getElementById("btnEn");
+var disableControls = document.getElementById("btnDis");
+var checkControls = document.getElementById("btnChe");
+var playVid = document.getElementById("btn1");
+var pauseVid = document.getElementById("btn2");
+var changeVid = document.getElementById("btn3");
+var forwVid = document.getElementById("btn4");
 
-function playVid() {
+
+
+playVid.onclick = function() {
 	vid.play();
-}
+};
 
-function pauseVid() {
+pauseVid.onclick = function() {
 	vid.pause();
-}
+};
 
-function enableControls() { 
+enableControls.onclick = function() { 
     vid.controls = true;
     vid.load();
-} 
+}; 
 
-function disableControls() { 
+disableControls.onclick = function() { 
     vid.controls = false;
     vid.load();
-} 
+}; 
 
-function checkControls() { 
+checkControls.onclick = function() { 
     alert(vid.controls);
-} 
+};
+
+changeVid.onclick = function() {
+
+    if (vid.getAttribute("src") == "media/vid2.mp4") {
+    vid.setAttribute("src", "media/vid1.mp4");
+    vid.load();
+    }
+
+    else {
+        vid.setAttribute("src", "media/vid2.mp4");
+        vid.load();
+    }
+
+    
+
+  
+};
+
+forwVid.onclick = function(){
+    vid.currentTime = 10;
+};
+
+
+
+
 
 vid.onpause = function() {
     alert("The video has been paused");
+};
+
+vid.onplay = function() {
+    alert("The video has been played");
+};
+
+vid.onvolumechange = function() {
+    alert("The volumen has been changed");
 };
 
 
@@ -36,15 +78,22 @@ vid.onpause = function() {
 
 
 
-
 var aud = document.getElementById("aud1");
+var play_audio = document.getElementById("btnAud1");
+var pause_audio = document.getElementById("btnAud2");
 
-function playAud() {
+/* function playAud() {
 	aud.play();
 }
 
 function pauseAud() {
 	aud.pause();
+} */
+
+play_audio.onclick = function(){
+    aud.play();
 }
 
-
+pause_audio.onclick = function(){
+    aud.pause();
+}
