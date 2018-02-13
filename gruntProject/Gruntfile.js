abb45fecf93f6aafa22c85ef88c0022974323391
David Siguenza Rodriguez <dsiguenzarod@gmail.com>
2018-02-13 15:01:40 +0100

    Grunt

---
+++ gruntProject/Gruntfile.js
@@ -0,0 +1,24 @@
+
+module.exports = function(grunt) {
+
+	// Configurar Grunt
+	grunt.initConfig({
+		// Leer el package.json
+		pkg: grunt.file.readJSON('package.json');
+	});
+
+	// Cargar plugins
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Definir tareas
+	
+	// Primera tarea: Validación de código
+	jshint: {
+		options: { reporter: require('jshint-stylish') },
+		build: ['Gruntfile.js','src/**/*.js']
+	}
+}
